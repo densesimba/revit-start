62b209c7c6b906415a5dc10e51e555f743cbaaa2
Denise Boghean <denis.boghean@leviatan.ro>
2022-11-17 17:44:48 +0200

    changes

---
+++ tra/tra/Room.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tra
+{
+   public class Room
+    {
+
+        public int index=0;
+        public float LocationX { get; set; }
+        public float LocationY { get; set; }
+
+        public float LengthX { get; set; }
+        public float LengthY { get; set; }
+        public string Name { get; set; }
+
+
+        
+
+
+
+       
+    }
+}
